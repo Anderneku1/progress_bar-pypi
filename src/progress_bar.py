dd0b77b376cd66feed1a721c9b63b7d46804b5ab
SiXFeet <68037779+Anderneku1@users.noreply.github.com>
2022-08-10 13:31:06 +0100

    Create progress_bar.py

---
+++ src/progress_bar.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+
+class progress_bar():
+    @staticmethod
+    def bar(progress, total, icon='█', begin='Downloading', show_percent=True):
+        '''
+        Be sure to print a ('\\n') after running the bar function!
+        You can find out why ;)
+        '''
+        percent = 100 * (progress / total)
+        bar = icon * int(percent) + '-' * (100 - int(percent))
+        if show_percent == True:
+            print(f'{begin} |{bar}| {percent:.2f}%', end='\r')
+        else:
+            print(f'{begin} |{bar}|', end='\r')
+   
+    @staticmethod
+    def rotate(begin = 'Downloading', sleep_value = 0.1):
+        list = ['|', '/', '-', '\\']
+        ting = '|'
+        for i in list:
+            sleep(sleep_value)
+            ting = i
+            print(f'\r{begin} {ting}', end='\r')
